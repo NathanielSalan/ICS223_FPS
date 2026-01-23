a2a6e1d05653297671667d390a17d38a4a7a6967
Nathan <nathanielsalan@gmail.com>
2026-01-22 22:00:17 -0800

    Started Movement

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class FPSInput : MonoBehaviour
+{
+    private float speed = 9.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    Vector3 movement = Vector3.forward * speed * Time.deltaTime;
+        //}
+
+    }
+}

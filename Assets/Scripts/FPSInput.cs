f47042089641e560ebe08a886b1c41909d202a7c
Nathan <nathanielsalan@gmail.com>
2026-01-27 12:13:19 -0800

    Fixed collision and camera

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
+    private float gravity = -9.8f;
     private float speed = 9.0f;
+    private CharacterController charController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -17,8 +19,23 @@ public class FPSInput : MonoBehaviour
         float horizonInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizonInput, 0, verticalInput) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizonInput, 0, verticalInput);
 
-        transform.Translate(movement);
+        // Clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+
+        // take speed into account
+        movement *= speed;
+
+        // Add gravity
+        movement.y = gravity;
+
+        // make movement processor independent
+        movement *= Time.deltaTime;
+
+        // Convert local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        charController.Move(movement);
     }
 }

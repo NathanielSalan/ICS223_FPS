fcdd9b4aaed7343e274b1a5f9d6c3f5514bce62d
Nathan <nathanielsalan@gmail.com>
2026-03-12 21:46:31 -0700

    Added UI Elements

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -45,15 +45,15 @@ public class RayShooter : MonoBehaviour
 
     }
 
-    void OnGUI()
-    {
-        GUIStyle style = new GUIStyle();
-        style.fontSize = aimSize;
-        // find the center of the camera view and adjust for asterisk
-        float posX = cam.pixelWidth / 2 - aimSize / 4;
-        float posY = cam.pixelHeight / 2 - aimSize / 2;
-        GUI.Label(new Rect(posX, posY, aimSize, aimSize), "*", style);
-    }
+    //void OnGUI()
+    //{
+    //    GUIStyle style = new GUIStyle();
+    //    style.fontSize = aimSize;
+    //    // find the center of the camera view and adjust for asterisk
+    //    float posX = cam.pixelWidth / 2 - aimSize / 4;
+    //    float posY = cam.pixelHeight / 2 - aimSize / 2;
+    //    GUI.Label(new Rect(posX, posY, aimSize, aimSize), "*", style);
+    //}
 
     private IEnumerator CreateTempSphereIndicator(Vector3 hitPosition)
     {

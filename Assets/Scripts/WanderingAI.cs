d4baa32de8602c22792171732cce5ca02dafb8e0
Nathan <nathanielsalan@gmail.com>
2026-03-12 19:48:10 -0700

    Added walk animation for Kyle

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -4,7 +4,7 @@ public enum EnemyStates { alive, dead};
 
 public class WanderingAI : MonoBehaviour
 {
-    private float enemySpeed = 3.0f;
+    [SerializeField] private float enemySpeed = 1.75f;
     private float obstacleRange = 5.0f;
     private float sphereRadius = 0.75f;
     private EnemyStates state;

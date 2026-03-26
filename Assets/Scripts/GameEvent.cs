d9c801731cd785587a2e2ab4fe2c81af05626484
Nathan <nathanielsalan@gmail.com>
2026-03-25 22:08:09 -0700

    Healthbar Goal Completed

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -4,4 +4,5 @@ public static class GameEvent
 {
     public const string ENEMY_DEAD = "ENEMY_DEAD";
     public const string DIFFICULTY_CHANGED = "DIFFICULTY_CHANGED";
+    public const string HEALTH_CHANGED = "HEALTH_CHANGED";
 }
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,21 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField] private int maxHealth = 5;
     private int health;
+
+    [SerializeField] private Image healthbar;
 
     // Use this for initialization
     void Start()
     {
-        health = 5;
+        health = maxHealth;
     }
+
+    private void Awake()
+    {
+        Messenger<float>.AddListener(GameEvent.HEALTH_CHANGED, OnHealthChanged);
+    }
+    private void OnDestroy()
+    {
+        Messenger<float>.RemoveListener(GameEvent.HEALTH_CHANGED, OnHealthChanged);
+    }
+
     public void Hit()
     {
         health -= 1;
         Debug.Log("Health: " + health);
+
+        float healthPercentage = (float)health / (float)maxHealth;
+
+        Debug.Log("Health %: " + healthPercentage);
+
+        Messenger<float>.Broadcast(GameEvent.HEALTH_CHANGED, healthPercentage);
+
         if (health == 0)
         {
             Debug.Break();
         }
+    }
+
+    public void OnHealthChanged(float percentage)
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, percentage);
+
+        healthbar.fillAmount = percentage;
+        healthbar.color = healthColor;
     }
 }
